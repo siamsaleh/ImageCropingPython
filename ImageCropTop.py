b065e6675aa977b6a6414ee906accbaa5f21117d
SiamSaleh <siamsaleh9@gmail.com>
2023-12-05 14:51:49 +0600

    top bottom image cropping added in ImageCroping.py

--- ImageCropTop.py
+++ ImageCropTop.py
@@ -14,18 +14,25 @@ for filename in os.listdir(folder_path):
 
 print(file_names)
 
+forCut = images[1][2: 117, 102: 750]
+string = "top-page-{:03d}".format(1)
+cv2.imshow(string, forCut)
+
+
 # Save Images In Folder
-x = 0
-for image in images:
-    forCut = image[117: 1145, 106: 748]  # image cropping axis values
-    string = "top-page-{:03d}".format(x)
-    cv2.imshow(string, forCut)
-    savedImageFolderPath = 'imagesTopBottom/'  # folder path where we will save the images
-    cv2.imwrite(os.path.join(savedImageFolderPath, file_names[x]), forCut)  # image saving in folder
-    x = x + 1
+# x = 0
+# for image in images:
+#     forCut = image[2: 117, 102: 750]  # image cropping axis values
+#     string = "top-page-{:03d}".format(x)
+#     cv2.imshow(string, forCut)
+#     savedImageFolderPath = 'imagesTopBottom/'  # folder path where we will save the images
+#     cv2.imwrite(os.path.join(savedImageFolderPath, file_names[x]), forCut)  # image saving in folder
+#     x = x + 1
+
+
+# border 3|1|4|2
+# TOP Corner(X(LEFT):Y(TOP)), Bottom Corner(X(RIGHT):Y(BOTTOM))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# border 3|1|4|2
-# TOP Corner(X(LEFT):Y(TOP)), Bottom Corner(X(RIGHT):Y(BOTTOM))
