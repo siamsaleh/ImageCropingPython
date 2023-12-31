7f6618db33e9a582148ab18f6464e8da3f083a33
SiamSaleh <siamsaleh9@gmail.com>
2023-12-31 16:19:41 +0600

    finalimages complete without visible left right text

--- image_adding.py
+++ image_adding.py
@@ -16,28 +16,68 @@ left_trans_images = support_class.read_left_trans_images()
 # for image in right_images:
 #     img_bg = Image.open(r"imageCroppedRight/" + right_file_names[index])
 #     img_over = Image.open(r"imagesTransparentRight/" + "trans-right-" + right_file_names[index])
+#     img_over_original_r = Image.open(r"imagesTransparentRight/" + "trans-right-" + right_file_names[index])
+#
+#     # Resize
+#     img_over_r = img_over_original_r.resize((int(img_over_original_r.width * .75), img_over_original_r.height),
+#                                             Image.ANTIALIAS)
 #
 #     image_bg_width = img_bg.width
-#     image_add_width = img_over.width
+#     image_add_width_r = img_over_r.width
 #
-#     img_bg.paste(img_over, (image_bg_width - image_add_width + 7, 0), mask=img_over)
+#     img_bg.paste(img_over_r, (image_bg_width - image_add_width_r + 7, 0), mask=img_over_r)
 #     img_bg.save("finalImages/" + right_file_names[index])
+#     # img_bg.paste(img_over, (image_bg_width - image_add_width + 7, 0), mask=img_over)
+#     # img_bg.save("finalImages/" + right_file_names[index])
 #
 #     print(index)
 #     index = index+1
 ########################################################################################################################
 # TODO adding LEFT images
-index = 0
-for image in left_images:
-    img_bg = Image.open(r"imageCroppedLeft/" + left_none_file_names[index])
-    img_over = Image.open(r"imagesTransparentLeft/" + "trans-left-" + left_none_file_names[index])
+# index = 0
+# for image in left_images:
+#     img_bg = Image.open(r"imageCroppedLeft/" + left_none_file_names[index])
+#     img_over = Image.open(r"imagesTransparentLeft/" + "trans-left-" + left_none_file_names[index])
+#     img_over_original_l = Image.open(r"imagesTransparentLeft/" + "trans-left-" + left_none_file_names[index])
+#
+#     # Resize
+#     img_over_l = img_over_original_l.resize((int(img_over_original_l.width * .75), img_over_original_l.height),
+#                                             Image.ANTIALIAS)
+#
+#     image_bg_width = img_bg.width
+#     image_add_width_l = img_over_l.width
+#
+#     img_bg.paste(img_over_l, (- 7, 0), mask=img_over_l)
+#     img_bg.save("finalImages/" + left_none_file_names[index])
+#     # img_bg.paste(img_over, (- 7, 0), mask=img_over)
+#     # img_bg.save("finalImages/" + left_none_file_names[index])
+#
+#     print(index)
+#     index = index+1
 
-    image_bg_width = img_bg.width
-    image_add_width = img_over.width
-
-    img_bg.paste(img_over, (- 7, 0), mask=img_over)
-    img_bg.save("finalImages/" + left_none_file_names[index])
-
-    print(index)
-    index = index+1
-
+# TEST
+########################################################################################################################
+# # TODO adding LEFT RIGHT images
+# index = 0
+# for image in all_images:
+#     # for image in range(0, 10):
+#     img_bg = Image.open(r"imagesCenter/" + image_file_names[index])
+#     img_over_original_r = Image.open(r"imagesTransparentRight/" + "trans-" + image_file_names[index])
+#     img_over_original_l = Image.open(r"imagesTransparentLeft/" + "trans-" + image_file_names[index])
+#
+#     # Resize
+#     img_over_r = img_over_original_r.resize((int(img_over_original_r.width * .75), img_over_original_r.height),
+#                                             Image.ANTIALIAS)
+#     img_over_l = img_over_original_l.resize((int(img_over_original_l.width * .75), img_over_original_l.height),
+#                                             Image.ANTIALIAS)
+#
+#     image_bg_width = img_bg.width
+#     image_add_width_r = img_over_r.width
+#     image_add_width_l = img_over_l.width
+#
+#     img_bg.paste(img_over_r, (image_bg_width - image_add_width_r + 7, 0), mask=img_over_r)
+#     img_bg.paste(img_over_l, (- 7, 0), mask=img_over_l)
+#     img_bg.save("finalImages/" + image_file_names[index])
+#
+#     print(index)
+#     index = index + 1
